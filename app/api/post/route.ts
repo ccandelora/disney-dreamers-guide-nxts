00446dea3dd7583ffaba942184669ab5dc91d9ff
Chris Candelora <chris.candelora@gmail.com>
2023-09-14 11:37:42 -0400

    removing post and delete from the post api untill auth is in place

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -16,34 +16,34 @@ export async function GET(req: Request) {
   }
 }
 
-export async function POST(req: Request) {
-    try {
-      const body: IPost = await req.json()
-      const newPost = new Post(body)
-      const saved = await newPost.save()
-      return NextResponse.json(saved)
-    } catch {
-      return NextResponse.json('error', {
-        status: 500
-      })
-    }
-  }
-  
-  export async function DELETE(req: Request) {
-    const query = new URL(req.url).searchParams
-    const id = query.get('id')
-    try {
-      const deletedPost = await Post.findByIdAndDelete(id)
-  
-      return NextResponse.json(deletedPost)
-    } catch {
-      return NextResponse.json(
-        {
-          error: 'Failed to remove post'
-        },
-        {
-          status: 500
-        }
-      )
-    }
-  }
+//export async function POST(req: Request) {
+//    try {
+//      const body: IPost = await req.json()
+//      const newPost = new Post(body)
+//      const saved = await newPost.save()
+//      return NextResponse.json(saved)
+//    } catch {
+//      return NextResponse.json('error', {
+//        status: 500
+//      })
+//    }
+//  }
+//  
+//  export async function DELETE(req: Request) {
+//    const query = new URL(req.url).searchParams
+//    const id = query.get('id')
+//    try {
+//      const deletedPost = await Post.findByIdAndDelete(id)
+//  
+//      return NextResponse.json(deletedPost)
+//    } catch {
+//      return NextResponse.json(
+//        {
+//          error: 'Failed to remove post'
+//        },
+//        {
+//          status: 500
+//        }
+//      )
+//    }
+//  }
